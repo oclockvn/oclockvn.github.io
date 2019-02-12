6f9f87235c9efdaa0d38203ae0036157be505870
Quang Phan <oclockvn@gmail.com>
2019-02-12 09:33:19 +0700

    added share buttons

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function() {
+    console.log(`%c
+    Contact me via:
+    P: 091.658.1461
+    E: oclockvn@gmail.com
+    G: gh.com/oclockvn
+    S: oclockvn`, 'background: #4caf50; color: #fff; font-size: 32px;');
+})();
